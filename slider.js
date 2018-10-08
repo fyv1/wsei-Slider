182f0ecb0d333247fd68dc4e4804e66b19aef3e6
Grzegorz Nowak <grzegorz.nowak.formal@gmail.com>
2018-10-08 20:42:30 +0200

    Added easier config in case if user wants to add or remove his photos

--- slider.js
+++ slider.js
@@ -1,16 +1,23 @@
 let position = 0;
 let elem = document.querySelector("#slides");
 
+// CONFIG VARS
+let imgWidth = 920;
+let amount = 3;
+
+
 function nextClick() {
-    if (position > -1840){
-        position -= 920;
+    if (position > -1 * imgWidth*(amount-1)){
+        position -= imgWidth;
         elem.style.transform = "translateX("+position+"px)";
-    }
+        
+        }
 } 
 
 function prevClick() {
     if (position < 0){
-        position += 920;
+        position += imgWidth;
         elem.style.transform = "translateX("+position+"px)";
     }
 }
+//TODO: at the end of 'queue' return to 1st/last img
