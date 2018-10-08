33818846f06a6142d573c7f5a4136dd06758da43
Grzegorz Nowak <grzegorz.nowak.formal@gmail.com>
2018-10-08 19:57:49 +0200

    Initial commit

---
+++ slider.js
@@ -0,0 +1,21 @@
+let position = 0;
+let elem = document.querySelector("#slides");
+
+function nextClick() {
+    if (position > -1840){
+        position -= 920;
+        elem.style.transform = "translateX("+position+"px)";
+    }
+} 
+
+function prevClick() {
+    if (position < 0){
+        position += 920;
+        elem.style.transform = "translateX("+position+"px)";
+    }
+}
+
+window.addEventListener('scroll', onscroll);
+function onscroll() {
+    console.log(window.scrollY);
+}
